34a35290de5cd9c8fdcd51c35201f6d1da25f3c4
Nikita Tsygankov <like_fender@mail.ru>
2023-04-02 18:11:09 +0300

    task 1 added

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+int Message(string msg)
+
+{
+    System.Console.WriteLine($"{msg}");
+    return int.Parse(Console.ReadLine());
+}
+int number = Message("Введите трехзначное число");
+if (number >=100 && number <1000)
+{
+int mid = (number % 100 - number % 10) / 10;
+System.Console.WriteLine($"Вторая цифра числа {number} является {mid}");
+}
+else
+System.Console.WriteLine("Это число не трехзначное");
