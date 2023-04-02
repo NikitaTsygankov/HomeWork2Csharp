16512490cc88a4f89b130a8e5bc6cec6d85ffb63
Nikita Tsygankov <like_fender@mail.ru>
2023-04-02 19:02:06 +0300

    task 3 added

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int Prompt(string msg)
+{
+
+System.Console.WriteLine($"{msg}");
+return int.Parse(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+string ss = "Это не день недели";
+string check(int number)
+{
+if (number >= 6 && number < 8) ss = "Это выходной";
+if (number >= 1 && number < 7) ss = "Это будний день";
+return ss;
+}
+System.Console.WriteLine(check(number));
